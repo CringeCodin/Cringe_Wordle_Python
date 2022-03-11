b788047bc1f4b60ff4871ed042bcafb00ca78314
James Haddix <74989150+CringeCodin@users.noreply.github.com>
2022-03-11 16:55:06 +0000

    everything

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # Cringe_Wordle_Python
 My version of Wordle.
-
-# How It Works
-no idea haven't done anything yet
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
 import random
 from termcolor import cprint
-from ast import literal_eval
 
 def isUniqueString(string):
 	uniqueCharSet = set()
@@ -10,19 +10,19 @@ def isUniqueString(string):
 		uniqueCharSet.add(char)
 	return True
 
-possibleGuessWordsIsolated = []
-possibleCorrectWords = []
 
 wordsFile = open("wordlist.txt")
-#GET WORDS HERE
 
+
+possibleWords = []
+nonUniqueWords = []
+
+for line in wordsFile:
+	if isUniqueString(line.rstrip("\n")):
+		possibleWords.append(line.rstrip("\n"))
+	else:
+		nonUniqueWords.append(line.rstrip("\n"))	
 wordsFile.close()
-possibleGuessWords = []
-for word in possibleGuessWordsIsolated:
-	possibleGuessWords.append(word)
-for word in possibleCorrectWords:
-	possibleGuessWords.append(word)
-
 
 guesses = 6
 numOfGuesses = 0
@@ -33,21 +33,24 @@ wordGuessed = False
 def restart():
 	global wordGuessed
 	global correctWord
-	# correctWord = random.choice(possibleCorrectWords)
-	correctWord = "robot"
+	try:
+		correctWord = random.choice(possibleWords)
+	except IndexError:
+		cprint("List empty.", "red")	
+		sys.exit()
 	wordGuessed = False
 def guess():
 	global numOfGuesses
 	playerGuess = 0
-	while not playerGuess in possibleGuessWords or not len(playerGuess) == 5:
+	while not playerGuess in possibleWords or not len(playerGuess) == 5:
 		playerGuess = input(f"Guess {numOfGuesses + 1}: ")
 		if not len(playerGuess) == 5:
 			cprint("Not 5 letters long", "red")
-		elif not playerGuess in possibleGuessWords:
+		elif not playerGuess in possibleWords:
 			cprint("Not an acceptable word", "red")
 		else:
-			evaluate(playerGuess)
 			numOfGuesses += 1
+			evaluate(playerGuess)
 def evaluate(guess):
 	global wordGuessed
 	correctLetters = 0
@@ -70,10 +73,10 @@ def evaluate(guess):
 				cprint(letter, "green")	
 			correctLetters += 1
 	if correctLetters == 5:
-		print(f"Correct! The word was {correctWord}")
+		cprint(f"Correct! The word was {correctWord}", "green")
 		wordGuessed = True
 	elif correctLetters != 5 and numOfGuesses == 6:
-		print(f"You lose! The word was {correctWord}")
+		cprint(f"You lose! The word was {correctWord}", "red")
 
 restart()
 for i in range(guesses):
@@ -81,3 +84,69 @@ for i in range(guesses):
 		guess()
 	else:
 		break
+
+
+"""
+HOW TO DO MULTIPLE LETTERS
+
+Idea 1: Iterate over word trying to find correct letters while
+keeping track of how many times each letter occurs using a dict
+
+Then try and find yellows then everything else is grey
+
+we could use a list storing the colors of each letter and then 
+iterate over each letter in the guess and then the list, ie
+correct is "beans"
+guess is "never"
+colorlist is ["white", "white", "white", "white", "white"]
+
+
+iterate over guess and check whether index we're on is equal to
+the index in the correct one
+if it is we change value in colorlist
+
+N, B
+not equal
+E, E
+equal, change colorlist[1] to green
+V, A
+not equal
+E, N
+not equal
+R, S
+not equal
+
+colorlist is now ["white", "green", "white", "white", "white"]
+
+now let's iterate over both again
+
+NB
+n is in beans, and the index of n in beans (4) does not already 
+equal guess[4], so change colorlist[0] to yellow
+EE
+ignore because colorlist[1] is not white
+VA
+v is not in beans
+EN
+e is in beans, but when we head to the earliest point it occurs, 
+we already have it in colorlist
+
+we can now iterate over beans inside this iteration to try and
+find an unmatched e
+
+there isn't any, so colorlist[3] is still white
+RS
+no r in beans
+
+
+never
+beans
+
+final colorlist is ["yellow", "green", "white", "white", "white"]
+
+for idx, i in enumerate((guess, colorlist)):
+	if idx != 4:
+		cprint(i[0], i[1], end="")
+	else:
+		cprint(i[0], i[1])
+"""
